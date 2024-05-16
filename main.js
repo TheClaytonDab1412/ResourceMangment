85197babb56308cd94371c7a6557a55bf0efdf86
TheClaytonDab1412 <claytongowen02@gmail.com>
2024-05-16 16:16:45 -0400

    added inventory and some test items

---
+++ main.js
@@ -0,0 +1,57 @@
+var item = {
+    item_name: "Default",
+    item_icon: "Default.png",
+    item_amount: 1,
+    
+    add_items: function(amount) {
+        this.item_amount += amount;
+        display.Update_Inventory()
+    },
+
+    remove_items: function(amount) {
+        this.item_amount -= amount;
+        display.Update_Inventory()
+    }
+};
+
+var item_list = {
+    item_name_list: [
+
+    ],
+    item_icon_tist: [
+
+    ],
+    item_amount_list: [
+
+    ],
+    make_item: function(item_name,item_icon) {
+        this.item_name_list[item_name] = item_name;
+
+        if(item_icon != null) {
+            this.item_icon_tist[item_name] = item_icon;
+        }
+        else {
+            this.item_icon_tist[item_name] = "images/Default.png";
+        }
+
+        this.item_amount_tist[item_name] = 1;
+    }
+};
+
+var display = {
+    Update_Inventory: function() {
+        document.getElementById("Inventory_container").innerHTML = "";
+        for (i = 0; i < item.item_name.length(); i++) {
+            document.getElementById("Inventory_container").innerHTML += '<table><dl class="item_slot"><dt id="name"><img id="image" src='+item.image[i]+'><p>'+item.name[i]+'</p></dt><dt id="amount"><p><span>'+item.amount[i]+'</span></p></dt></dl></table>';
+        }
+    }
+};
+
+window.onload = function() {
+    display.Update_Inventory();
+};
+
+var make_items = function() {
+    item_list.make_item("IronOre","images/Default.png")
+    item_list.make_item("Iron","images/Default.png")
+}
