f514a9d3d1f568718cff00566ac3a637ab663ec5
TheClaytonDab1412 <claytongowen02@gmail.com>
2024-05-18 16:46:04 -0400

    more ui changes

--- js/inventory.js
+++ js/inventory.js
@@ -41,7 +41,7 @@ Inventory = function() {
         var str = "";
         for (var i = 0; i < self.items.length; i++){
             let item = Item.List[self.items[i].id];
-            str += "<button>" + item.name + " x" + self.items[i].amount + "</button><br>";
+            str += "<p>" + item.name + " x" + self.items[i].amount + "</p><br>";
         }
         document.getElementById("Inventory_Container").innerHTML = str;
     }
