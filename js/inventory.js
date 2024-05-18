ab30a2797f985681df942de7304cad8414866fb4
TheClaytonDab1412 <claytongowen02@gmail.com>
2024-05-18 17:04:53 -0400

    remove it so when a item is 0 its slot is removed

--- js/inventory.js
+++ js/inventory.js
@@ -19,15 +19,10 @@ Inventory = function() {
         for (i = 0; i < self.items.length; i++){
             if(self.items[i].id === id){
                 self.items[i].amount -= amount;
-                if(self.items[i].amount <= 0){
-                    self.items.splice(i,1);
-                    self.refreshRender();
-                }
+                self.refreshRender();
                 return;
             }
         }
-        self.items.push({id:id,amount:amount});
-        self.refreshRender();
     }
     self.hasItem = function(id,amount){
         for (i = 0; i < self.items.length; i++){
