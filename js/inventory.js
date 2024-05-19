59152ff8f5a7bfa55d0beb887fb5f4cbfb3f11d2
TheClaytonDab1412 <claytongowen02@gmail.com>
2024-05-19 11:31:26 -0400

    change were div is in item_slot

--- js/inventory.js
+++ js/inventory.js
@@ -36,7 +36,7 @@ Inventory = function() {
         var str = "";
         for (var i = 0; i < self.items.length; i++){
             let item = Item.List[self.items[i].id];
-            str += "<div class=item_slot><img src="+item.image_path+" "+"class=item_img>" + "<p class=item_text></div>" + item.name + " x" + self.items[i].amount + "</p><br>";
+            str += "<div class=item_slot><img src="+item.image_path+" "+"class=item_img>" + "<p class=item_text>" + item.name + " x" + self.items[i].amount + "</p></div><br>";
         }
         document.getElementById("Inventory_Container").innerHTML = str;
     }
