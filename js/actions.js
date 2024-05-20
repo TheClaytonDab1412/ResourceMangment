f5942c692e2c82d00c7426d7ddf31a64ff1c74c9
TheClaytonDab1412 <claytongowen02@gmail.com>
2024-05-19 22:22:19 -0400

    stuff

--- js/actions.js
+++ js/actions.js
@@ -2,12 +2,12 @@ ActionList = function(){
     var self = {
         actions:[] //{id:"actionID",event:onclick}
     }
-    self.UnlockAction = function(id,event) {
+    self.UnlockAction = function(id) {
         for (i = 0; i < self.actions.length; i++) {
             if(self.actions[i].id === id){
                return;
             }
-            self.actions.push({id:id,event:event});
+            self.actions.push({id:id});
             self.refreshRender();
         }
     };
@@ -23,7 +23,7 @@ ActionList = function(){
         var str = "";
         for (var i = 0; i < self.actions.length; i++){
             let action = action.List[self.actions[i].id];
-            str += "<div class=action_slot><img src="+action.image_path+" "+"class=action_img>" + "<button onclick=" +action.event+ "class=action_text>" + action.name + " x" + self.actions[i].amount + "</button></div>";
+            str += "<button onclick="+action.event+" "+"class=action_slot> <img src="+action.image_path+" "+"class=action_img>"+"<p class=action_text>"+action.name+"<p></button>";
         }
         document.getElementById("Action_Container").innerHTML = str;
     };
@@ -43,6 +43,6 @@ action = function(id,name,image_path,event){
 }
 action.List = {};
 
-action("mine_iron_ore","Mine Iron Ore","images/iron_ore.png",function() {
+action("mine_iron_ore","Mine Iron Ore","images/iron_ore.png",function(){
     playerInventory.addItem("iron_ore",1)
 })
